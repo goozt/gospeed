3c18f4869d874c7c041162d0c37f597ea55e3a26
Nikhil John <nikhiljohn1010@gmail.com>
2026-03-19 10:40:12 +0530

    feat: enhance versioning by integrating build info retrieval for version, commit, and date

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,8 +1,37 @@
 package version
 
+import "runtime/debug"
+
 // Set via ldflags at build time.
 var (
 	Version = "dev"
 	Commit  = "unknown"
 	Date    = "unknown"
 )
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	// When installed via `go install`, the module version is set automatically.
+	if Version == "dev" && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	}
+
+	if Commit == "unknown" {
+		for _, s := range info.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				if len(s.Value) > 7 {
+					Commit = s.Value[:7]
+				} else {
+					Commit = s.Value
+				}
+			case "vcs.time":
+				Date = s.Value
+			}
+		}
+	}
+}
